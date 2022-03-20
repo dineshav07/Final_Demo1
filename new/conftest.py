555c6811167be3884a8365ac111b0af761d4c325
dinesh <dineshkumarav07@gmail.com>
2022-03-20 23:22:39 +0530

    demo

--- new/conftest.py
+++ new/conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
