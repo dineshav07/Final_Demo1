e82cb07b7a19b1071a4c13b2b5b4427c4c79fad3
dinesh <dineshkumarav07@gmail.com>
2022-03-20 21:55:55 +0530

    demo

--- new/conftest.py
+++ new/conftest.py
@@ -9,7 +9,6 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-__import__(test_runner)
 
 driver = None
 
